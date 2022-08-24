5f929c03c7195923f7af09a95a86472b3b4aa68d
Nandaayuputriashari <nandaayuputriashari@gmail.com>
2022-08-25 06:48:58 +0700

    nanda

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>CRUD - PHP & MySQL + Modal Bootstrap 5</title>
+    <title>DATA MAHASISWA</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
 
@@ -21,8 +21,8 @@
     <div class="container">
 
     <div class="mt-3">
-    <h3 class="text-center">CRUD - PHP & MySQL + Modal Bootstrap 5</h3>
-    <h3 class="text-center">Ngodingpintar</h3>
+    <h3 class="text-center">DATA MAHASISWA</h3>
+    <h3 class="text-center">POLITEKNIK NEGERI BANYUWANGI</h3>
     </div>
 
     <div class="card mt-3">
@@ -46,17 +46,106 @@
                 <th>Aksi</th>
             </tr>
 
+            <?php
+            
+            //persiapan menampilkan data
+            $no = 1;
+            $tampil = mysqli_query($koneksi, "SELECT * FROM tmhs ORDER BY id_mhs DESC");
+            while($data = mysqli_fetch_array($tampil)):
+            
+            ?>
             <tr>
-                <td>1</td>
-                <td>362155401098</td>
-                <td>Nanda Ayu Putri Ashari</td>
-                <td>Singojuruh</td>
-                <td>D4 - TRPL</td>
+                <td><?=$no++ ?></td>
+                <td><?=$data['nim']?></td>
+                <td><?=$data['nama']?></td>
+                <td><?=$data['alamat']?></td>
+                <td><?=$data['prodi']?></td>
                 <td>
-                    <a href="#" class="btn btn-warning">Ubah</a>
-                    <a href="#" class="btn btn-danger">Hapus</a>
+                    <a href="#" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#modalUbah<?= $no ?>">Ubah</a>
+                    <a href="#" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modalHapus<?= $no ?>">Hapus</a>
                 </td>
             </tr>
+
+            <!-- Awal Modal Ubah -->
+<div class="modal fade" id="modalUbah<?= $no ?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Form Data Mahasiswa</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <form method="POST" action="aksi_crud.php">
+        <input type="hidden" name="id_mhs" value="<?=$data['id_mhs']?>">
+      <div class="modal-body">
+        
+        <div class="mb-3">
+  <label class="form-label">NIM</label>
+  <input type="text" class="form-control" name="tnim" value="<?=$data['nim']?>"
+  placeholder="Masukkan NIM Anda!">
+</div>
+
+<div class="mb-3">
+  <label class="form-label">Nama Lengkap</label>
+  <input type="text" class="form-control" name="tnama" value="<?=$data['nama']?>"
+  placeholder="Masukkan Nama Lengkap Anda!">
+</div>
+
+<div class="mb-3">
+  <label class="form-label">Alamat</label>
+  <textarea class="form-control" name="talamat" rows="3"><?=$data['alamat']?></textarea>
+</div>
+
+<div class="mb-3">
+  <label class="form-label">Prodi</label>
+  <select class="form-select" name="tprodi">
+    <option value="<?=$data['prodi']?>"><?=$data['prodi']?></option>
+    <option value="D4 - Teknologi Rekayasa Perangkat Lunak">D4 - Teknologi Rekayasa Perangkat Lunak</option>
+    <option value="D4 - Teknologi Rekayasa Perangkat Komputer">D4 - Teknologi Rekayasa Perangkat Komputer</option>
+    <option value="D4 - Manajemen Bisnis Pariwisata">D4 - Manajemen Bisnis Pariwisata</option>
+
+  </select>
+ </div>
+
+      </div>
+      <div class="modal-footer">
+      <button type="submit" class="btn btn-primary" name="bubah" >Ubah</button>
+        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Keluar</button>
+              </div>
+      </form>
+    </div>
+  </div>
+</div>
+<!-- Akhir Modal Ubah -->
+
+           <!-- Awal Modal Hapus -->
+           <div class="modal fade" id="modalHapus<?= $no ?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Konfirmasi Hapus Data</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <form method="POST" action="aksi_crud.php">
+        <input type="hidden" name="id_mhs" value="<?=$data['id_mhs']?>">
+      
+        <div class="modal-body">
+    
+        <h5 class="text-center">Apakah anda yakin akan menghapus data ini? <br>
+        <span class="text-danger"><?=$data['nim']?> - <?=$data['nama']?></span>
+    </h5>
+
+      </div>
+      <div class="modal-footer">
+      <button type="submit" class="btn btn-primary" name="bhapus" >Silahkan Dihapus!</button>
+        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Keluar</button>
+              </div>
+      </form>
+    </div>
+  </div>
+</div>
+<!-- Akhir Modal Hapus -->
+
+            <?php endwhile; ?>
         </table>
 
 
