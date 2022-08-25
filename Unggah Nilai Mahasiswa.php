1649388506d5447f98ef33a9aac7059c0caddb58
azka <masazka512@gmail.com>
2022-08-25 11:52:27 +0700

    azkaazkiya

--- Unggah Nilai Mahasiswa.php
+++ Unggah Nilai Mahasiswa.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <link rel="stylesheet" type="teks/css" href="admind.css">
    
-    <title>Hello, world!</title>
+    <title>mahasiswa</title>
   </head>
   <body>
    
